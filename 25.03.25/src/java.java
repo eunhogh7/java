ff59675bacb0a8c296edc74fc76fe68ebebaa31c
ken <cju050803@gmail.com>
2025-03-25 11:32:01 +0900

    d

---
+++ 25.03.25/src/java.java
@@ -0,0 +1,8 @@
+public class java {
+    public static void main(String[] args) {
+        float radius = 10.0f;
+        final float PI = 3.14f;
+        System.out.println(radius*radius*PI);
+        System.out.println("반지름이 10인 원의 면적은 314이다");
+    }
+}
